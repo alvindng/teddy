94534e60578a26cd818b0c3ecbfa394db926d984
Alvin Ng and Noah Ramey <noah@>
2016-05-31 11:35:51 -0700

    added buttons to change background color

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(".btn-default").click(function() {
+  $("body").removeClass();
+  $("body").addClass("white-black")
+})
+
+$(".btn-primary").click(function() {
+  $("body").removeClass();
+  $("body").addClass("black-white")
+})
